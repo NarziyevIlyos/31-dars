ffaafc927a8d66dafe9b94c653f19027b810cdef
Ilyos <ilyosnarziyev6@gmail.com>
2024-11-21 02:09:15 +0500

    31-darsni yukladim

---
+++ js/index.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            primary: "#1e40af",
+            brownish: "#D67E34",
+            btn_bg1_color: "#FF7400",
+            btn_bg2_color: "#D67E34",
+            dark_gray_text: "#656363",
+            white_text: "#FFFFFF"
+        },
+        container: {
+          center: true,
+          screens: {
+            xl: "1110px",
+          },
+        },
+        boxShadow: {
+          'custom-card': '10px 10px 40px 20px #0000001A', // Custom shadow
+        },
+      },
+    },
+  };
+  
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//     theme: {
+//       extend: {
+//         boxShadow: {
+//           'custom-card': '10px 10px 40px 20px #0000001A', // Custom shadow
+//         },
+//       },
+//     },
+//     plugins: [],
+//   };
+  
